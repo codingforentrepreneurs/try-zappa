a66f9849e2b07cd36eb3c89664f07fdea1d8835c
CFE <hello@teamcfe.com>
2018-11-14 15:31:56 -0800

    19 - Update Django for PostgreSQL

--- zapdj/settings.py
+++ zapdj/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'zappa_django_utils',
 ]
 
 MIDDLEWARE = [
@@ -73,13 +74,23 @@ WSGI_APPLICATION = 'zapdj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'zapdjangodb',
+        'USER': 'zapdjangouser',
+        'PASSWORD': '15422715-547f-4eec-93b5-faa4f90e1d48',
+        'HOST': 'zapdjangodb.cfak8zfgryqc.us-west-2.rds.amazonaws.com',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
